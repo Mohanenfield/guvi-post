677277aea23a0689943453986e33450a4807511c
Mohanenfield <83965863+Mohanenfield@users.noreply.github.com>
2021-08-10 15:24:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const { MongoAPIError } = require("mongodb");
 const app = express();
 require("dotenv").config();
-const mongo = require("./shared/mongo")
 const cors =require("cors");
+const postsRouter = require("./routes/posts.route");
+const mongo = require("./shared/mongo")
+
 async function loadApp() {
     try {
         await mongo.connect();
-
-        const postsRouter = require("./routes/posts.route");
+ app.use(cors());
+        
         app.use(express.json());
-        app.use(cors());
+       
         app.use("/posts", postsRouter);
         app.listen(3001, () => console.log("Server is running at Port 3001"));
     } catch (err) {
